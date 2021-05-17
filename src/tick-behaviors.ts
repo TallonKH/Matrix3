8f8a86283046fbe60a04fdc6e2212e6ffb98cf54
Tallon Hodge <tallonkh@gmail.com>
2021-05-17 14:43:46 -0500

    renamed `swap` to `displace`, and created separate functions for single and double displacements

--- src/tick-behaviors.ts
+++ src/tick-behaviors.ts
@@ -11,7 +11,7 @@ const UP_LEFT = new NPoint(-1, 1);
 const DOWN_RIGHT = new NPoint(1, 1);
 const UP_RIGHT = new NPoint(1, -1);
 
-export const updateMultiSwap: (offsets: Array<NPoint>, fallback: TickBehavior) => TickBehavior =
+export const updateDisplaceN: (offsets: Array<NPoint>, fallback: TickBehavior) => TickBehavior =
   (offsets, fallback) =>
     (world, chunk, i) => {
       const targets: Array<[Chunk, number]> = [];
@@ -44,27 +44,67 @@ export const updateMultiSwap: (offsets: Array<NPoint>, fallback: TickBehavior) =
       world.setBlockData(last[0], last[1], chunk.getBlockData(i));
     };
 
-export const updateSwap: (offset: NPoint, fallback: TickBehavior) => TickBehavior =
-  (offset, fallback) => updateMultiSwap([offset], fallback);
+export const updateDisplace1: (offset: NPoint, fallback: TickBehavior) => TickBehavior =
+  // (offset, fallback) => updateDisplaceN([offset], fallback);
+  // ^ this works, but is slower
+  (offset, fallback) => (world, chunk, i) => {
+    const targetCI = chunk.getNearIndexI(i, offset.x, offset.y);
+    if (targetCI === null
+      || targetCI[0].getFlagOfBlock(targetCI[1], UpdateFlags.LOCKED)
+      || world.getDensityOfBlock(targetCI[0], targetCI[1]) >= world.getDensityOfBlock(chunk, i)) {
+      fallback(world, chunk, i);
+      return;
+    }
+
+    world.setBlockData(targetCI[0], targetCI[1], chunk.getBlockData(i));
+    world.setBlockData(chunk, i, targetCI[0].getBlockData(targetCI[1]));
+  };
+
+export const updateDisplace2: (offset1: NPoint, offset2: NPoint, fallback: TickBehavior) => TickBehavior =
+  // (offset1, offset2, fallback) => updateDisplaceN([offset1, offset2], fallback);
+  // ^ this works, but is slower
+  (offset1, offset2, fallback) => (world, chunk, i) => {
+    const selfDensity = world.getDensityOfBlock(chunk, i);
+
+    const target1CI = chunk.getNearIndexI(i, offset1.x, offset1.y);
+    if (target1CI === null
+      || target1CI[0].getFlagOfBlock(target1CI[1], UpdateFlags.LOCKED)
+      || world.getDensityOfBlock(target1CI[0], target1CI[1]) >= selfDensity) {
+      fallback(world, chunk, i);
+      return;
+    }
+
+    const target2CI = chunk.getNearIndexI(i, offset2.x, offset2.y);
+    if (target2CI === null
+      || target2CI[0].getFlagOfBlock(target2CI[1], UpdateFlags.LOCKED)
+      || world.getDensityOfBlock(target2CI[0], target2CI[1]) >= selfDensity) {
+      fallback(world, chunk, i);
+      return;
+    }
+
+    world.setBlockData(target2CI[0], target2CI[1], chunk.getBlockData(i));
+    world.setBlockData(target1CI[0], target1CI[1], target2CI[0].getBlockData(target2CI[1]));
+    world.setBlockData(chunk, i, target1CI[0].getBlockData(target1CI[1]));
+  };
 
 export const updateFall: (fallback: TickBehavior) => TickBehavior =
-  (fallback) => updateSwap(DOWN, fallback);
+  (fallback) => updateDisplace1(DOWN, fallback);
 
 export const updateCrumble: (fallback: TickBehavior) => TickBehavior =
   (fallback) => updateFall((w, c, i) => {
     if (w.getRandomFloat() > 0.5) {
-      updateMultiSwap([LEFT, DOWN_LEFT], fallback)(w, c, i);
+      updateDisplace2(LEFT, DOWN_LEFT, fallback)(w, c, i);
     } else {
-      updateMultiSwap([RIGHT, DOWN_RIGHT], fallback)(w, c, i);
+      updateDisplace2(RIGHT, DOWN_RIGHT, fallback)(w, c, i);
     }
   });
 
 export const updateCascade: (fallback: TickBehavior) => TickBehavior =
   (fallback) => updateFall((w, c, i) => {
     if (w.getRandomFloat() > 0.5) {
-      updateMultiSwap([LEFT, DOWN_LEFT], updateMultiSwap([RIGHT, DOWN_RIGHT], fallback))(w, c, i);
+      updateDisplace2(LEFT, DOWN_LEFT, updateDisplace2(RIGHT, DOWN_RIGHT, fallback))(w, c, i);
     } else {
-      updateMultiSwap([RIGHT, DOWN_RIGHT], updateMultiSwap([LEFT, DOWN_LEFT], fallback))(w, c, i);
+      updateDisplace2(RIGHT, DOWN_RIGHT, updateDisplace2(LEFT, DOWN_LEFT, fallback))(w, c, i);
     }
   });
 
@@ -72,9 +112,9 @@ export const updateFlow: (moveChance: number, fallback: TickBehavior) => TickBeh
   (moveChance, fallback) => updateCascade((w, c, i) => {
     if (w.getRandomFloat() <= moveChance) {
       if (w.getRandomFloat() > 0.5) {
-        updateSwap(LEFT, updateSwap(RIGHT, fallback))(w, c, i);
+        updateDisplace1(LEFT, updateDisplace1(RIGHT, fallback))(w, c, i);
       } else {
-        updateSwap(RIGHT, updateSwap(LEFT, fallback))(w, c, i);
+        updateDisplace1(RIGHT, updateDisplace1(LEFT, fallback))(w, c, i);
       }
     } else {
       w.queueBlock(c, i);
