34d532c76ea4ab5a1cacfb643b1e584c890de35d
Tallon Hodge <tallonkh@gmail.com>
2021-05-26 00:27:23 -0500

    better draw type selection

--- src/threading/hub.ts
+++ src/threading/hub.ts
@@ -206,7 +206,7 @@ for (let i = 0; i < standardBlockTypes.length; i++) {
   button.classList.add("block-type-button");
   button.style.backgroundColor = btype.color.toHex();
   button.innerHTML = btype.name;
-  button.addEventListener("click", (e) => {
+  button.addEventListener("mousedown", (e) => {
     if (e.button === 0) {
       for (const other of buttons) {
         other.classList.remove("selected-primary");
