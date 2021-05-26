c36f1993a8799e83760ce48b0d7b5d737360e611
Tallon Hodge <tallonkh@gmail.com>
2021-05-25 21:19:30 -0500

    grass → dirt behavior

--- src/standard/block-types.ts
+++ src/standard/block-types.ts
@@ -1,6 +1,7 @@
 import { Color } from "../library";
 import { updateCascade, updateCrumble, updateFall, updateFlow } from "./block-behaviors";
-import BlockType, { densityConstant, updateStatic } from "../simulation/matrix-blocktype";
+import BlockType, { densityConstant, TickBehavior, updateStatic } from "../simulation/matrix-blocktype";
+import World from "../simulation/matrix-world";
 
 export const standardBlockTypes: Array<BlockType> = [];
 
@@ -37,7 +38,25 @@ standardBlockTypes.push(new BlockType({
   name: "grass",
   color: Color.fromHex("#4eeb10"),
   densityFunc: densityConstant(200),
-  tickBehaviorGen: () => updateFall(updateStatic),
+  tickBehaviorGen: (world: World): TickBehavior => {
+    const airMat = world.getBlockTypeIndex("air") ?? 0;
+    const dirtMat = world.getBlockTypeIndex("dirt") ?? 0;
+
+    return (world, chunk, i) => {
+      // if no dirt below, become dirt
+      const below = chunk.getNearIndexI(i, 0, -1);
+      if (below !== null && below[0].getTypeOfBlock(below[1]) !== dirtMat) {
+        world.tryMutateTypeOfBlock(chunk, i, dirtMat);
+        return;
+      }
+      // if no air above, become dirt
+      const above = chunk.getNearIndexI(i, 0, 1);
+      if (above !== null && above[0].getTypeOfBlock(above[1]) !== airMat) {
+        world.tryMutateTypeOfBlock(chunk, i, dirtMat);
+        return;
+      }
+    };
+  },
 }));
 
 standardBlockTypes.push(new BlockType({
